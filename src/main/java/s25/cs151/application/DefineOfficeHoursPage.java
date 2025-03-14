fc647986178085162ff6a21ba5270b07b5a48abc
vaip05 <vaispanc@gmail.com>
2025-03-13 19:00:14 -0700

    Initial commit - Upload ProfMeet project

---
+++ src/main/java/s25/cs151/application/DefineOfficeHoursPage.java
@@ -0,0 +1,79 @@
+package s25.cs151.application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import java.util.*;
+
+public class DefineOfficeHoursPage {
+    private final Stage stage;
+    private final VBox view;
+    private final Parent previousPage;
+
+    public DefineOfficeHoursPage(Stage stage, Parent previousPage) {
+        this.stage = stage;
+        this.view = View();
+        this.previousPage = previousPage;
+    }
+
+    private VBox View() {
+        VBox vbox = new VBox(20);
+        Label title = new Label("Define Semester Office Hours");
+        GridPane form = new GridPane();
+
+        Label semesterLabel = new Label("Semester: ");
+        ComboBox<String> semesterCombo = new ComboBox<>();
+        semesterCombo.getItems().addAll("Spring", "Summer", "Fall", "Winter");
+        form.add(semesterLabel, 0, 0);
+        form.add(semesterCombo, 1, 0);
+
+        Label yearLabel = new Label("Year: ");
+        TextField yearField = new TextField();
+        form.add(yearLabel, 0 ,1);
+        form.add(yearField, 1, 1);
+
+        Label daysLabel = new Label("Days: ");
+        VBox daysBox = new VBox(5);
+        CheckBox mon = new CheckBox("Monday");
+        CheckBox tue = new CheckBox("Tuesday");
+        CheckBox wed = new CheckBox("Wednesday");
+        CheckBox thu = new CheckBox("Thursday");
+        CheckBox fri = new CheckBox("Friday");
+        daysBox.getChildren().addAll(mon, tue, wed, thu, fri);
+        form.add(daysLabel, 0, 2);
+        form.add(daysBox, 1, 2);
+
+        Button saveButton = new Button("Save");
+        Button backButton = new Button("Back");
+        HBox buttonBox= new HBox(backButton, saveButton);
+
+        backButton.setOnAction(e -> stage.setScene(new Scene(previousPage, 700, 700))
+            //HomePage home = new HomePage(stage);
+            //stage.setScene(new Scene(home.getView(), 700, 700));
+        );
+
+        saveButton.setOnAction(e -> {
+            String semester = semesterCombo.getValue();
+            String year = yearField.getText();
+            List<String> daysSelected = new ArrayList<>();
+
+            if (semester == null || year.isEmpty()) {
+                showAlert("");
+                return;
+            }
+        });
+
+        vbox.getChildren().addAll(title, form, buttonBox);
+        return vbox;
+    }
+
+    public Parent getView() {
+        return view;
+    }
+    private void showAlert(String alert){
+        
+    }
+}
---
+++ src/main/java/s25/cs151/application/HomePage.java
@@ -0,0 +1,96 @@
+package s25.cs151.application;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+
+public class HomePage{
+
+    private Button defineSemOfficeHours;
+    private Button defineSemSlots;
+    private Button defineNewCourse;
+    private Button settingsBtn;
+    private Button scheduleBtn;
+    private Button viewAllBtn;
+    private Button searchBtn;
+    private Stage homeStage;
+
+    public HomePage(Stage homeStage){
+        this.homeStage = homeStage;
+    }
+    public BorderPane getView(){
+
+        //title text
+        Text title = new Text("ProfMeet");
+        //HBox titleBox = new HBox(title);
+
+        //might need to change this portion so title text is not in middle
+        title.setStyle("-fx-font-size: 40px; -fx-font-weight: bold");
+
+        //sidebar of application
+        VBox sidebar = new VBox(20);
+        sidebar.setStyle("-fx-background-color: linear-gradient(to bottom, #5D7B6F, #EAE7D6);");
+        sidebar.setAlignment(javafx.geometry.Pos.CENTER);
+
+        //create buttons
+        defineSemOfficeHours = new Button("Define Semester Office Hours");
+        //go to define sem office hours page on click
+        defineSemOfficeHours.setOnAction(e -> {
+            DefineOfficeHoursPage definePage = new DefineOfficeHoursPage(homeStage, this.getView());
+            homeStage.setScene(new Scene(definePage.getView(), 700, 700));
+        });
+        defineSemSlots = new Button("Define Semester Time Slots");
+        defineNewCourse = new Button("Define New Course");
+        settingsBtn = new Button("Settings");
+
+        //stylize sidebar buttons
+        defineSemOfficeHours.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+        defineSemSlots.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+        defineNewCourse.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+        settingsBtn.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+
+
+        //add all buttons to sidebar vbox
+        sidebar.getChildren().addAll(title, defineNewCourse, defineSemSlots, defineSemOfficeHours, settingsBtn);
+
+        //header and styling
+        Text headerMsg = new Text("Welcome, Professor! Here's your office hour overview: ");
+        HBox header = new HBox(headerMsg);
+        header.setStyle("-fx-background-color: #D7F9FA; -fx-font-size: 15px; -fx-alignment: center;;");
+
+        //main content
+        VBox appointmentPg = new VBox(20);
+        appointmentPg.setStyle("-fx-background-color: #A4C3A2");
+
+        HBox actionButtons = new HBox(20);
+
+        scheduleBtn = new Button("Schedule a new Office Hour");
+        viewAllBtn = new Button("View All Appointments");
+        searchBtn = new Button("Search for Appointments");
+
+        //stylize the buttons
+        scheduleBtn.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+        viewAllBtn.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+        searchBtn.setStyle("-fx-background-color: #E2DFDA; -fx-text-fill: grey; -fx-font-size: 14px; -fx-font-weight: bold;");
+
+        actionButtons.getChildren().addAll(scheduleBtn, viewAllBtn, searchBtn);
+
+        appointmentPg.getChildren().add(actionButtons);
+
+        BorderPane root = new BorderPane();
+        root.setTop(header);
+        root.setLeft(sidebar);
+        root.setCenter(appointmentPg);
+
+        return root;
+    }
+
+}
